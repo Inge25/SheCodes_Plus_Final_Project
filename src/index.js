81f0ee77421da4d0bea4371b80ac494b2f12323a
Inge25 <117174569+Inge25@users.noreply.github.com>
2025-02-18 17:29:06 +0100

    integrated weather API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
+function refreshWeatherData(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5a85355e7783a33t5694043ffeoa1b50";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeatherData);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#city-search-form");
 searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("Amsterdam");
